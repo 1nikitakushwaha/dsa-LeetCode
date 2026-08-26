fdd22337a8671340ae905b282b5c2ebf8861a29b
Nikita Kushwaha <1nikita.kushwaha@gmail.com>
2026-08-26 14:02:15 +0530

    Time: 0 ms (100%), Space: 19 MB (98.42%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,15 +5,27 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #  **brute force solution tc=o(n) sc=o(1)**
+        # count=0
+        # curr=head
 
-        count=0
-        curr=head
+        # while curr!= None:
+        #     count+=1
+        #     curr=curr.next
+        # middle=count//2
+        # curr=head
+        # for i in range(0,middle):
+        #     curr=curr.next
+        # return curr
 
-        while curr!= None:
-            count+=1
-            curr=curr.next
-        middle=count//2
-        curr=head
-        for i in range(0,middle):
-            curr=curr.next
-        return curr
+        #OPTINAL SOLUTION
+        slow=head
+        fast=head
+
+        while fast is not None and fast.next is not None:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
+
+
+    
