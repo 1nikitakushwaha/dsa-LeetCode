60bb1b5515961d3a31e7fbed65f80496bc78a340
Nikita Kushwaha <1nikita.kushwaha@gmail.com>
2026-09-18 22:15:46 +0530

    Time: 0 ms (100%), Space: 19.3 MB (65.51%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,19 +5,36 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode | None, n: int) -> ListNode | None:
-        length=0
-        temp=head
-        while temp is not None:
-            length+=1
-            temp=temp.next
-        if length==n:
-            new_head=head.next
-            return new_head
-        pos=length-n
-        count=1
-        temp=head
-        while count<pos:
-            temp=temp.next
-            count+=1
-        temp.next=temp.next.next
+
+        ##brute force!!!!!!## tc=o(n) and sc=o(1) but we want in one traverse
+        # length=0
+        # temp=head
+        # while temp is not None:
+        #     length+=1
+        #     temp=temp.next
+        # if length==n:
+        #     new_head=head.next
+        #     return new_head
+        # pos=length-n
+        # count=1
+        # temp=head
+        # while count<pos:
+        #     temp=temp.next
+        #     count+=1
+        # temp.next=temp.next.next
+        # return head
+    
+    ##optimal solution
+        fast=head
+        slow=head
+        for i in range(n):
+            fast=fast.next
+        if fast is None:
+            return head.next
+        while fast.next is not None:
+            slow=slow.next
+            fast=fast.next
+        slow.next=slow.next.next
         return head
+
+        
